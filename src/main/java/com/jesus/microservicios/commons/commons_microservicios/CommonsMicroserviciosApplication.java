1a12ce4f4eb60abdb7c95b4ef1d999ed89ac99bd
Jesusmslformacion <chechuelcrack@gmail.com>
2026-04-09 15:08:00 +0200

    Agregar archivos de configuración y estructura inicial para el microservicio commons

---
+++ src/main/java/com/jesus/microservicios/commons/commons_microservicios/CommonsMicroserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.jesus.microservicios.commons.commons_microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsMicroserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsMicroserviciosApplication.class, args);
+	}
+
+}
