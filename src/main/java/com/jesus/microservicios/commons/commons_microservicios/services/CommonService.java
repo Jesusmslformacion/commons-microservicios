57957126427c37d503a7b43c5be96c7bafd15668
Jesusmslformacion <chechuelcrack@gmail.com>
2026-04-14 13:23:11 +0200

    Agregar interfaz y clase de implementación para el servicio Alumno

---
+++ src/main/java/com/jesus/microservicios/commons/commons_microservicios/services/CommonService.java
@@ -0,0 +1,16 @@
+package com.jesus.microservicios.commons.commons_microservicios.services;
+
+import java.util.Optional;
+
+
+public interface CommonService<E> {
+
+
+    public Iterable<E> findAll();
+
+    public Optional<E> findById(Long id);
+
+    public E save(E entity);
+
+    public void deleteById(Long id);
+}
