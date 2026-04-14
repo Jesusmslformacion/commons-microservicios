3c3ec534a4836c95713a8dec9e2224d158305139
Jesusmslformacion <chechuelcrack@gmail.com>
2026-04-15 00:49:49 +0200

    Eliminar anotación @Service y cambiar el modificador de acceso de 'private' a 'protected' en el repositorio de CommonServiceImpl

--- src/main/java/com/jesus/microservicios/commons/commons_microservicios/services/CommonServiceImpl.java
+++ src/main/java/com/jesus/microservicios/commons/commons_microservicios/services/CommonServiceImpl.java
@@ -4,15 +4,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
-@Service
+
 public class CommonServiceImpl<E, R extends CrudRepository<E, Long>> implements CommonService<E>{
 
     @Autowired
-    private R repository;
+    protected R repository;
 
     @Override
     @Transactional(readOnly = true)
